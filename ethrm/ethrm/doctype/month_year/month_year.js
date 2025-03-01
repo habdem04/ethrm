fd0e29ec1db696fe2b44f2d19b389547ca00ccdf
habdem04 <habdem2003@gmail.com>
2025-03-01 20:30:15 +0300

    Initial migrate

---
+++ ethrm/ethrm/doctype/month_year/month_year.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ELIF TECHNOLOGIES PLC and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Month Year", {
+// 	refresh(frm) {
+
+// 	},
+// });
