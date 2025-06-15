f4ebbf538a6cf0fdf4f288d624c9871547cdf203
habdem04 <habdem2003@gmail.com>
2025-06-15 10:38:00 +0000

    Update the Addition of Custom Client Script

--- ethrm/hooks.py
+++ ethrm/hooks.py
@@ -270,31 +270,7 @@ doc_events = {
 #     }
 # }
 
-# ethrm/hooks.py
-
 fixtures = [
-    # --- Export specific Client Scripts ---
-    {
-        "dt": "Client Script",
-        "filters": [
-            ["name", "in", [
-                "CS_Sales_Order_Alerts",
-                "CS_Item_Default_Values",
-                # add any other CS names here
-            ]]
-        ]
-    },
-    # --- Export specific Custom Fields ---
-    {
-        "dt": "Custom Field",
-        "filters": [
-            ["name", "in", [
-                # ERPNext names Custom Fields as "<DocType>-<field_name>"
-                "Employee-ethnicity",
-                "Employee-national_id",
-                "Leave Application-ethrm_approval_level",
-                # add your other custom field names here
-            ]]
-        ]
-    }
+    "Client Script",
+    "Custom Field"
 ]
